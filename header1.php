b1d2d386902db20d725a1487c589f67b6860bd52
pedrocavaliere <pedrokuchpil@gmail.com>
2017-05-25 14:51:33 -0300

    Segundo código a ser inserido no header.php (MESMO ARQUIVO DO OUTRO)
    Inseri-lo dentro do <body> do header

---
+++ header1.php
@@ -0,0 +1,4 @@
+
+<a href="javascript:void(0);" onclick="resizeText(1)" id="plustext">Aumentar texto</a>
+| 
+<a href="javascript:void(0);" onclick="resizeText(-1)" id="minustext">Diminuir texto</a>
